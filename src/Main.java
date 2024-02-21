ed18ea74c5e0e5a0b5dca790a216674635ff5abc
LucasLucky1 <lucaserickson100@gmail.com>
2024-02-21 12:13:46 -0300

    adicionando funcionalidades basicas

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in); //inicializando scanner
 
-        double saldoInicial = 2500.00;
+        double saldo = 2500.00;
+
 
         //Captando nome do banco e do cliente
         System.out.println("Olá, qual seu nome?");
@@ -14,13 +16,13 @@ public class Main {
         System.out.println("Bem vindo ao Banco "+nomeBanco + ", " + nomeCliente);
 
         //configurando mensagem de boas vindas
-        java.lang.String welcomeMessage = String.format("""
+        String welcomeMessage = String.format("""
                 ***********************
                 Dados iniciais do cliente:
                                 
                 Nome: %s
                 Tipo conta: Corrente
-                Saldo inicial: R$ %f
+                Saldo: R$ %f
                 ***********************
                                 
                 Operações
@@ -31,10 +33,60 @@ public class Main {
                 4- Sair
                                 
                 Digite a opção desejada:
-                """,nomeCliente,saldoInicial);
+                """,nomeCliente,saldo);
+
+        String shortMessage = String.format("""
+                
+                
+                ***********************          
+                Operações
+                                
+                1- Consultar saldos
+                2- Receber valor
+                3- Transferir valor
+                4- Sair
+                
+                *********************** 
+                """,saldo);
+
 
         System.out.println(welcomeMessage);
-//        int opcaoSelecionada = scanner.nextInt();
+        int opcaoSelecionada;
+        opcaoSelecionada= scanner.nextInt();
+
+        while(opcaoSelecionada <=4){
+
+            if(opcaoSelecionada == 1){
+                System.out.println("Você tem $" + saldo + " de saldo.");
+                System.out.println(shortMessage);
+                opcaoSelecionada= scanner.nextInt();
+
+
+
+            }else if(opcaoSelecionada == 2) {
+                System.out.println("Quanto você irá receber?");
+                double recebimento = scanner.nextDouble();
+                saldo=saldo+recebimento;
+                System.out.println("Você tem $" + saldo + " de saldo.");
+                System.out.println(shortMessage);
+                opcaoSelecionada= scanner.nextInt();
+
+
+            }else if(opcaoSelecionada == 3) {
+                System.out.println("Quanto você irá transferir?");
+                double transferencia = scanner.nextDouble();
+                saldo=saldo-transferencia;
+                System.out.println("Você tem $" + saldo + " de saldo.");
+
+                System.out.println(shortMessage);
+                opcaoSelecionada= scanner.nextInt();
+
+            }else if(opcaoSelecionada == 4) {
+                System.out.println("Obrigado por usar nosso banco!");
+                break;
+            }
+
+        }
 
 
     }
